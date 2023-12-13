ecff0a689fef5f90c12072fa9345e90e26968d91
Developer Antonio Alonso <antonio.alonso8012@gmail.com>
2023-12-13 11:45:39 -0600

    update

--- globe.js
+++ globe.js
@@ -98,7 +98,7 @@ DAT.Globe = function (container, opts) {
         shader = Shaders['earth'];
         uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-        uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'globe/assets/world.png');
+        uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'globe_v2/assets/world.png');
        /* uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'assets/world.png');*/
 
         material = new THREE.ShaderMaterial({
